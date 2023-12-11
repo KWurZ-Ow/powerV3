92af67bf2ba66e92eda8562d214163cf5aae8da5
KWurZ-Ow <marius.proton@gmail.com>
2023-12-11 14:02:52 +0100

    keyboard and back

--- src/pages/Mobile.tsx
+++ src/pages/Mobile.tsx
@@ -4,7 +4,8 @@ import "../app.css";
 import { useParams } from 'react-router-dom';
 import { childProps } from "../App";
 import playerIcon from "../media/playerIcon.svg"
-import KeyboardWrapper from '../components/KeyboardWrapper';
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
 
 export default function Mobile({ ioUrl }: childProps) {
     const { id: tableId } = useParams()
@@ -44,17 +45,80 @@ export default function Mobile({ ioUrl }: childProps) {
         socket?.emit("register", tableId, color)
     }, [color, socket, tableId])
 
-    const [input, setInput] = useState("");
-    const keyboard = useRef(null);
+    type inputsType = {
+        [cle: string]: string | number;
+    }
 
-    const onChangeInput = (event: ChangeEvent<HTMLInputElement>): void => {
-        const input = event.target.value;
-        setInput(input);
+    // Keyboard
+
+    const [inputs, setInputs] = useState<inputsType>({});
+    const [inputName, setInputName] = useState(0);
+
+    const onChangeAll = (inputs: any) => {
+        const upperCased: any = {};
+        Object.keys(inputs).forEach((cle) => {
+            upperCased[cle] = inputs[cle].toUpperCase();
+        });
+
+        setInputs({ ...upperCased });
     };
+    const onChangeInput = (event: any) => {
+        const inputVal = event.target.value;
+        setInputs({
+            ...inputs,
+            [inputName]: inputVal.toUpperCase()
+        });
+    };
+    const getInputValue = (inputName: number) => {
+        return inputs[inputName] || ""
+    };
+    const onKeyPress = (button: any) => {
+        if (button === "{nxt}") {
+            if (inputName > 13) {
+                setInputName(0)
+            } else {
+                setInputName(inputName + 1)
+            }
+        }
+        if (button === "{prev}") {
+            if (inputName < 1) {
+                setInputName(14)
+            } else {
+                setInputName(inputName - 1)
+            }
+        }
+    };
+    const customLayout = {
+        'default': [
+            'S R E 0 1 2',
+            'C B M 3 4 5',
+            'D C R 6 7 8',
+            'P H Q s I {bksp}',
+            'V b J r {prev} {nxt}'
+        ]
+    }
+    let customDisplay = {
+        '{bksp}': '←',
+        '{prev}': '⇦',
+        '{nxt}': '⇨',
+        's': 'S',
+        'b': 'B',
+        'r': 'R'
+    }
+    let buttonTheme = [
+        {
+            class: "keyGrey",
+            buttons: "S R C B D C R"
+        },
+        {
+            class: "keyLightgrey",
+            buttons: "0 1 2 3 4 5 6 7 8"
+        }
+    ]
 
     if (!table) return <h1>Scanne le QR code</h1>
 
-    return <div className='mobile'>
+    return <div className={`mobile ${color}`}>
         <h1>Power V3 !!!</h1>
         {color === "" ? <>
             <h2>Bienvenue sur la table "{table.name}"</h2>
@@ -73,15 +137,32 @@ export default function Mobile({ ioUrl }: childProps) {
         </> :
             <>
                 <h2>Sur la table "{table.name}"</h2>
-                <input
-                    value={input}
-                    placeholder={"Tap on the virtual keyboard to start"}
-                    onChange={e => onChangeInput(e)}
-                />
-                <div style={{ color: "black" }}>
-                    <KeyboardWrapper keyboardRef={keyboard as any} onChange={setInput} />
-                </div>
+                {[...Array(15)].map((_, i) => (
+                    <input
+                        key={i}
+                        value={getInputValue(i)}
+                        onFocus={() => setInputName(i)}
+                        className={inputName === i ? "focus" : ""}
+                        onChange={onChangeInput}
+                    />
+                ))}
+                <button onClick={() => { 
+                    socket?.emit("unRegister", tableId, color, (table:any) => {
+                        setTable(table)
+                    })
+                    setColor("")
+                 }}>Retour</button>
 
             </>}
+        <Keyboard
+            layoutName={"default"}
+            onChangeAll={onChangeAll}
+            layout={customLayout}
+            display={customDisplay}
+            theme={`hg-theme-default myTheme1 ${color === "" ? "" : "active"}`}
+            inputName={String(inputName)}
+            onKeyPress={onKeyPress}
+            buttonTheme={buttonTheme}
+        />
     </div>
 }
